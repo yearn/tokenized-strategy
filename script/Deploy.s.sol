cbc008276c4a16d4b02f3d30adb39c74e7ca5543
Schlagonia <schlagonia@gmail.com>
2023-10-07 14:31:29 -0600

    fix: deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,6 +6,8 @@ import "forge-std/Script.sol";
 // Deploy a contract to a deterministic address with create2
 contract Deploy is Script {
 
+    Deployer public deployer = Deployer(0x8D85e7c9A4e369E53Acc8d5426aE1568198b0112);
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
@@ -14,18 +16,20 @@ contract Deploy is Script {
         bytes memory bytecode = vm.getCode("TokenizedStrategy.sol:TokenizedStrategy");
 
         // Pick an unique salt
-        uint256 salt = uint256(keccak256("v3.0.0"));
+        uint256 salt = uint256(keccak256("v3.0.1"));
 
-        address contractAddress = deploy(bytecode, salt);
+        address contractAddress = deployer.deploy(bytecode, salt);
 
         console.log("Address is ", contractAddress);
 
         vm.stopBroadcast();
     }
+}
 
+contract Deployer {
     event Deployed(address addr, uint256 salt);
 
-    function deploy(bytes memory code, uint256 salt) public returns (address) {
+    function deploy(bytes memory code, uint256 salt) external  returns (address) {
         address addr;
         assembly {
             addr := create2(0, add(code, 0x20), mload(code), salt)
