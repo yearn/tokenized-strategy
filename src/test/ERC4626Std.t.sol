bbabc726b82041743f1a43642d4352f01a34b3e7
Schlag <89420541+Schlagonia@users.noreply.github.com>
2024-09-09 15:06:23 -0600

    feat: max deposit (#99)
    * feat: max uint deposit
    
    * fix: invariant test
    
    * fix: rebase
    
    * fix: dont max mint
    
    * fix: uint 256
    
    Co-authored-by: FP <83050944+fp-crypto@users.noreply.github.com>
    
    * fix: uint 256
    
    Co-authored-by: FP <83050944+fp-crypto@users.noreply.github.com>
    
    * test: check balances
    
    ---------
    
    Co-authored-by: FP <83050944+fp-crypto@users.noreply.github.com>

--- src/test/ERC4626Std.t.sol
+++ src/test/ERC4626Std.t.sol
@@ -15,4 +15,22 @@ contract ERC4626StdTest is ERC4626Test, Setup {
         _vaultMayBeEmpty = true;
         _unlimitedAmount = true;
     }
+
+    //Avoid special case for deposits of uint256 max
+    function test_previewDeposit(
+        Init memory init,
+        uint256 assets
+    ) public override {
+        if (assets == type(uint256).max) assets -= 1;
+        super.test_previewDeposit(init, assets);
+    }
+
+    function test_deposit(
+        Init memory init,
+        uint256 assets,
+        uint256 allowance
+    ) public override {
+        if (assets == type(uint256).max) assets -= 1;
+        super.test_deposit(init, assets, allowance);
+    }
 }
